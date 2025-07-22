2ac9a45e9bfad6646cb80bbe53fb88ae32373fb5
Clara Uhm <c@Mac.lan>
2025-07-21 23:16:47 -0400

    Created the basics of the project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # minimot
+
+I love data and I love Youtube, so this is a passion project.
---
+++ src/wordcloud_gen.py
@@ -0,0 +1,5 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+def generate_wordcloud(vtt_files, output_path="output/wordcloud.png"):
+    
